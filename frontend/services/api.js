73fe05228df8302cd58ca346c23ccf2a127b6e36
braulioygg01 <151060603+braulioygg01@users.noreply.github.com>
2024-06-19 21:06:55 -0600

    Delete frontend directory

--- frontend/services/api.js
+++
@@ -1,8 +0,0 @@
-// services/api.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Reemplaza con la URL de tu servidor backend
-});
-
-export default api;
